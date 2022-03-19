dd82166c66ec505ac138b792b947610f5c901ac6
ElJuanito <juan.serna@sjsu.edu>
2022-03-19 15:32:43 -0700

    edits

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let roll = ['#20', '#19', '#18', '#17', '#16', '#15', '#14', '#13', '#12', '#11', '#10', '#9', '#8', '#7', '#6', '#5', '#4', '#3', '#2', '#1'];
+let roll = ['You'];
 
-let action = ['Grab a', 'Throw a', 'Jump over a', 'Run into a', 'Pick up a', 'Conjure a', 'Walk away from a', 'Duck a', 'Use a', 'Swing a'];
+let action = ['grab a', 'throw a', 'jump over a', 'run into a', 'pick up a', 'conjure a', 'walk away from a', 'duck a', 'use a', 'swing a'];
 
 let weapon = ['axe', 'potion bottle', 'long sword', 'warhammer', 'drink of elixir', 'crossbow', 'morning star', ' short sword', 'gaunlet', 'helmet'];
 
-let actiont = [', throw at', ', run into', ', hide from', ', attack', ', cast a spell at', ', vanquish ', ', misses ', ', blocked by']
+let actiont = [',  and throw it at', ', and run into', ', and hide from', ', attack', ', cast a spell at', ',and vanquish ', ',it misses ', ', it is blocked by']
 
 
 let creature = ['a troll', 'a gargoyle', 'a serpant', 'a witch', 'a dark elf', 'a sorcerer', 'a fairy', 'a cave gnome', 'a barbarian', 'a fighter', 'a paladin', 'a ranger'];
@@ -19,6 +19,8 @@ let aet;
 let cr;
 let myFont;
 let wrds;
+let button;
+let centerPicture;
 //let canvas;
 
 
@@ -28,25 +30,30 @@ function preload() {
 }
 
 function setup() {
+
   fill('255');
   textFont(myFont);
   textSize(25);
-  createCanvas(1100, 1100);
+  createCanvas(1000, 1000);
   background(66, 40, 14);
   frameRate(60);
   intText();
+  button = createButton("Click to start your next Adventure")
+  button.mousePressed(buttonPressed);
+  button.class ("randomizerButton");
+
   //let canvas = createCanvas(windowWidth *0.5, (windowWidth*0.5)*1.250);
 
 }
 
 function draw() {
-  loadImage('ASSETS/dierolls/newdie2.png', img => {
-    image(img, width * 0.4, width * 0.4, width * 0.2, width * 0.2, );
+  loadImage('ASSETS/background/Tavernbackground.jpeg', img => {
+    image(img, width * 0.2, width * 0.26, width * 0.6, width * 0.4, );
   });
 }
 
 
-function mousePressed() {
+function buttonPressed() {
 
   dr = int(random(roll.length));
   ac = int(random(action.length));
@@ -70,14 +77,16 @@ function intText() {
   fill(247, 255, 0);
   textAlign(CENTER);
 
-  text('Roll the twenty sided die to start your next adventure.', width * 0.53, height * 0.25);
+  text('You walk out of a Tavern, click to see what happens next?', width * 0.50, height * 0.16);
 }
+
+
 
 function fortText() {
   background(66, 40, 14);
   intText();
   fill(247, 255, 0);
   textAlign(CENTER);
-  text('You rolled a', width * 0.5, height * 0.75);
+  text('Your next adventure begins', width * 0.5, height * 0.75);
   text(wrds, width * 0.5, height * 0.85)
 }
